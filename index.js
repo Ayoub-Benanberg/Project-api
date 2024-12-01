acb2c52881c34e52fe3883aed6b1aff669500ced
Ayoub-Benanberg <ayoubvufci@gmail.com>
2024-12-01 23:18:21 +0100

    removing react icon

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ const projects = [
                 image: {
                     src: "https://cdn.iconscout.com/icon/free/png-256/react-1-282599.png",
                     alt: "React-Logo",
-                    reactIcon: <FaReact/>
                 }
             },
             {
@@ -34,7 +33,6 @@ const projects = [
                 image: {
                     src: "https://cdn.iconscout.com/icon/free/png-256/tailwindcss-3521648-2945278.png",
                     alt: "Tailwind-CSS-Logo",
-                    reactIcon: <RiTailwindCssFill />
 
                 }
             }
@@ -59,7 +57,6 @@ const projects = [
                 image: {
                     src: "https://cdn.iconscout.com/icon/free/png-256/react-1-282599.png",
                     alt: "React-Logo",
-                    reactIcon: <FaReact/>
 
                 }
             },
@@ -68,7 +65,6 @@ const projects = [
                 image: {
                     src: "https://cdn.iconscout.com/icon/free/png-256/tailwindcss-3521648-2945278.png",
                     alt: "Tailwind-CSS-Logo",
-                    reactIcon: <RiTailwindCssFill />
 
                 }
             }
@@ -93,7 +89,6 @@ const projects = [
                 image: {
                     src: "https://cdn.iconscout.com/icon/free/png-256/html5-40-1175193.png",
                     alt: "HTML-Logo",
-                    reactIcon: <FaHtml5 />
 
                 }
             },
@@ -102,7 +97,6 @@ const projects = [
                 image: {
                     src: "https://cdn.iconscout.com/icon/free/png-256/css3-11-1175239.png",
                     alt: "CSS-Logo",
-                    reactIcon: <FaCss3 />
 
                 }
             },
@@ -111,7 +105,6 @@ const projects = [
                 image: {
                     src: "https://cdn.iconscout.com/icon/free/png-256/bootstrap-226077.png",
                     alt: "Bootstrap-Logo",
-                    reactIcon: <FaBootstrap />
 
                 }
             }
